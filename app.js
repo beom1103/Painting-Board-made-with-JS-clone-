c25e294ca04e89eff3605bbe1725c28d3f125626
3기-1반 애범 <gogot1103@naver.com>
2021-10-24 12:12:11 +0900

    색 변경

--- app.js
+++ app.js
@@ -1,4 +1,20 @@
 const canvas = document.getElementById("jsCanvas");
+const ctx = canvas.getContext('2d');
+const colors = document.getElementsByClassName("jsColor")
+
+canvas.width = 700;
+canvas.height = 700;
+
+
+//색상이나 스타일을 라인에(초기값을 검정색으로)
+ctx.strokestyle = "#2c2c2c";
+
+//색상이나 스타일을 쉐입에
+
+//그냥 선(path)
+
+//선의 굵기
+ctx.lineWidth = 2.5;
 
 let painting = false;
 
@@ -6,22 +22,38 @@ function stopPainting() {
   painting = false;
 }
 
-function onMouseMove(event) {
-  const x = event.offsetX;
-  const y = event.offsetY;
-}
-
-function onMouseDown(event) {
+function startPainting() {
   painting = true;
 }
 
-function onMouseUp(event) {
-  stopPainting();
+function onMouseMove(event) {
+  const x = event.offsetX;
+  const y = event.offsetY;
+  if (!painting) {
+    ctx.beginPath();
+    ctx.moveTo(x, y);
+  } else {
+    ctx.lineTo(x, y);
+    ctx.stroke();
+  }
 }
+
+// ctx.beginPath(); //경로 생성
+// ctx.moveTo(x, y); //선 시작 좌표
+// ctx.lineTo(x, y); //선 끝 좌표
+// ctx.stroke(); //선 그리기
+
+function handleColorClick(event) {
+  const color = event.target.style.backgroundColor;
+  ctx.strokeStyle = color;
+}
+
 
 if (canvas) {
   canvas.addEventListener("mousemove", onMouseMove);
-  canvas.addEventListener("mousedown", onMouseDown);
-  canvas.addEventListener("mouseup", onMouseUp);
+  canvas.addEventListener("mousedown", startPainting);
+  canvas.addEventListener("mouseup", stopPainting);
   canvas.addEventListener("mouseleave", stopPainting);
 }
+
+Array.from(colors).forEach(color => color.addEventListener("click", handleColorClick))
