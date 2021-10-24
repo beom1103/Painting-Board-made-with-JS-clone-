595f6392624e18d73df2d5b5ec900a97af6d7e25
3기-1반 애범 <gogot1103@naver.com>
2021-10-24 15:28:03 +0900

    brush size

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const canvas = document.getElementById("jsCanvas");
 const ctx = canvas.getContext('2d');
 const colors = document.getElementsByClassName("jsColor")
+const range = document.getElementById("jsRange");
+const mode = document.getElementById("jsMode");
 
 canvas.width = 700;
 canvas.height = 700;
@@ -17,6 +19,7 @@ ctx.strokestyle = "#2c2c2c";
 ctx.lineWidth = 2.5;
 
 let painting = false;
+let filling = false;
 
 function stopPainting() {
   painting = false;
@@ -48,6 +51,20 @@ function handleColorClick(event) {
   ctx.strokeStyle = color;
 }
 
+function handleRangeChange(event) {
+  const size = event.target.value;
+  ctx.lineWidth = size
+}
+
+function handleModeClick() {
+  if (filling === true) {
+    filling = false;
+    mode.innerText = "Fill"
+  } else {
+    filling = true;
+    mode.innerText = "Paint"
+  }
+}
 
 if (canvas) {
   canvas.addEventListener("mousemove", onMouseMove);
@@ -57,3 +74,11 @@ if (canvas) {
 }
 
 Array.from(colors).forEach(color => color.addEventListener("click", handleColorClick))
+
+if (range) {
+  range.addEventListener("input", handleRangeChange);
+}
+
+if (mode) {
+  mode.addEventListener("click", handleModeClick);
+}
